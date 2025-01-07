dac498dacce703bd0757ca2ee3df8d89ae8e84f1
adinizleadtax <adiniz.leadtax@out.bionexo.com>
2025-01-07 07:25:08 -0300

    🚧 migrations update

--- database/migrations/2025_01_07_101707_create_instrumentos_table.php
+++ database/migrations/2025_01_07_101707_create_instrumentos_table.php
@@ -9,19 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('artistas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('instrumentos', function (Blueprint $table) {
+        $table->id();
+        $table->string('nome');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('artistas');
+        Schema::dropIfExists('instrumentos');
     }
 };
